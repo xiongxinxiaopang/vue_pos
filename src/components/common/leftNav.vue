e82703ad48560ce75098d5f814f4f0fa4c6cbff3
luxiongxin <471574522@qq.com>
2017-10-30 17:08:09 +0800

    first

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="left-nav">
+    <ul>
+      <li>
+        <i class="icon iconfont icon-goumai"></i>
+        <span>收银</span>
+      </li>
+      <li>
+        <i class="icon iconfont icon-dianpu"></i>
+        <span>店铺</span>
+      </li>
+      <li>
+        <i class="icon iconfont icon-hanbao"></i>
+        <span>商品</span>
+      </li>
+      <li>
+        <i class="icon iconfont icon-huiyuanqia"></i>
+        <span>会员</span>
+      </li>
+      <li>
+        <i class="icon iconfont icon-paixing"></i>
+        <span>统计</span>
+      </li>
+      <li>
+        <i class="icon iconfont icon-gongnengjianyi"></i>
+        <span>设置</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.left-nav {
+  float: left;
+  width: 5%;
+  height: 100%;
+  font-size: 10px;
+  background-color: #1d8ce0;
+  color: #fff;
+}
+.left-nav ul {
+  list-style: none;
+  margin: 0px;
+  padding: 0px;
+}
+.left-nav li {
+  text-align: center;
+  border-bottom: 1px solid #20a0ff;
+  padding: 10px;
+}
+.left-nav li span {
+  display: block;
+}
+.left-nav .iconfont {
+  font-size: 24px;
+}
+</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="pos">
+    <el-row>
+      <el-col :span="7" class="pos-order" id="order-list">
+        <el-tabs>
+          <el-tab-pane label="点餐">
+            <el-table :data="tableData" border style="width: 100%">
+              <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+              <el-table-column prop="count" label="数量" width="70"></el-table-column>
+              <el-table-column prop="price" label="金额" width="70"></el-table-column>
+              <el-table-column label="操作" fixed="right">
+                <template slot-scope="scope">
+                  <el-button type="text" size="small">删除</el-button>
+                  <el-button type="text" size="small">增加</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <div class="btn-wrap">
+              <el-button type="warning">挂单</el-button>
+              <el-button type="danger">删除</el-button>
+              <el-button type="success">结账</el-button>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="挂单">挂单</el-tab-pane>
+          <el-tab-pane label="外卖">外卖</el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col :span="17" class="poc-prodc">
+        <div class="often-goods">
+          <h3 class="title">常用商品</h3>
+          <div class="often-goods-list">
+            <ul>
+              <li v-for="goods in oftenGoods" :key="goods.id">
+                <span>{{goods.goodsName}}</span>
+                <span class="o-price">￥{{goods.price}}元</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="goods-type">
+          <el-tabs>
+            <el-tab-pane label="汉堡">
+              <ul class="cookList">
+                  <li v-for="goods in typeOneFoods" :key="goods.id">
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+              </ul>
+            </el-tab-pane>
+            <el-tab-pane label="小食">小食</el-tab-pane>
+            <el-tab-pane label="饮料">饮料</el-tab-pane>
+            <el-tab-pane label="套餐">套餐</el-tab-pane>
+          </el-tabs>
+        </div>
+      </el-col>  
+    </el-row>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "pos",
+  data() {
+    return {
+      tableData: [
+        {
+          goodsName: "可口可乐",
+          price: 8,
+          count: 1
+        },
+        {
+          goodsName: "香辣鸡腿堡",
+          price: 15,
+          count: 1
+        },
+        {
+          goodsName: "爱心薯条",
+          price: 8,
+          count: 1
+        },
+        {
+          goodsName: "甜筒",
+          price: 8,
+          count: 1
+        }
+      ],
+      oftenGoods: [],
+      typeOneFoods: []
+    };
+  },
+  created() {
+    axios
+      .get("http://jspang.com/DemoApi/oftenGoods.php")
+      .then(response => {
+        if (response.status == 200) {
+          this.oftenGoods = response.data;
+        }
+      })
+      .catch(error => {
+        this.$message.error("服务器错误");
+      });
+    axios
+      .get("http://jspang.com/DemoApi/typeGoods.php")
+      .then(response => {
+        this.typeOneGoods = response[0];
+      })
+      .catch(error => {
+        this.$message({
+          showClose: true,
+          message: "错了哦，这是一条错误消息",
+          type: "error"
+        });
+      });
+  },
+  mounted() {
+    let orderHeight = document.body.clientHeight;
+    document.getElementById("order-list").style.height = orderHeight + "px";
+  }
+};
+</script>
+<style scoped>
+.pos-order {
+  background-color: #f9fafc;
+  border-right: 1px solid #c0ccda;
+}
+.btn-wrap {
+  margin-top: 40px;
+}
+.title {
+  font-size: 14px;
+  height: 20px;
+  margin: 0px;
+  font-weight: 300;
+  border-bottom: 1px solid #d3dce6;
+  background-color: #f9fafc;
+  padding: 10px;
+  text-align: left;
+}
+.often-goods-list ul li {
+  list-style: none;
+  float: left;
+  border: 1px solid #e5e9f2;
+  padding: 10px;
+  margin: 10px;
+  background-color: #fff;
+}
+.o-price {
+  color: #58b7ff;
+}
+.goods-type {
+  clear: both;
+}
+.cookList li {
+  list-style: none;
+  width: 23%;
+  border: 1px solid #e5e9f2;
+  height: auot;
+  overflow: hidden;
+  background-color: #fff;
+  padding: 2px;
+  float: left;
+  margin: 2px;
+}
+.cookList li span {
+  display: block;
+  float: left;
+}
+.foodImg {
+  width: 40%;
+}
+.foodName {
+  font-size: 18px;
+  padding-left: 10px;
+  color: brown;
+}
+.foodPrice {
+  font-size: 16px;
+  padding-left: 10px;
+  padding-top: 10px;
+}
+</style>
